517776fe759de56a46712f3e8f18abeda0274e57
Jesicka Labud <jesicka@gmail.com>
2014-01-31 05:08:47 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tencompliments::Application.config.secret_key_base = '809e28a345482169f4b6b35582ac3cb4bf3b2d5b2a4c2a0d2f1e9437cfd6430ec7a802c7df57fe159b79253718e8b77e1b01906f789ac13b0fd7d690e959957c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tencompliments::Application.config.session_store :cookie_store, key: '_tencompliments_session'
